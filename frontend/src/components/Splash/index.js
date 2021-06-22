606f14b06707f5d73fea0f0aaa583c0eee097bf4
Geoffrey <geoffreyyang@gmail.com>
2021-06-22 13:51:31 -0400

    splash image done

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -6,16 +6,26 @@ import Button from './Button'
 import './Splash.css'
 
 export default function Splash() {
-    const [showModal, setShowModal]=useState(false)
+    // const [showModal, setShowModal] = useState(false)
 
-    const openModal =()  =>   {
-        setShowModal(prev => !prev)
-    };
+    // const openModal = () => {
+    //     setShowModal(prev => !prev)
+    // };
 
     return (
-        <div className = "splash_background" >
-            <Button />
-        </div>
+        <>
+            <div className="splash__container">
+                <div className="splash__text__wrapper">
+                    <div className="splash__text">
+                        <h1 className="cover-heading" id="cover-heading" >Do you need a gift?</h1>
+                        <p className="lead" id="opener">Gift Now is why you'll never gift the same way again.</p>
+                        <p className="lead">
+                            {/* <a className="btn btn-lg btn-default" ">Get started</a> */}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
 
     )
 }
