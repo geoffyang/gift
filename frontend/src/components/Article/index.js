7b15db51cee8cf5cddfeef3acc9d4816c6c725c4
Geoffrey <geoffreyyang@gmail.com>
2021-06-23 01:12:54 -0400

    start article component

---
+++ frontend/src/components/Article/index.js
@@ -0,0 +1,15 @@
+
+export default function Article({ image, content }) {
+
+    return (
+        <li className="article">
+            <div className="article__image-wrapper">
+                {/* <img src={image} */}
+            </div>
+            <div className="article__content-wrapper">
+
+            </div>
+        </li>
+    )
+
+}
