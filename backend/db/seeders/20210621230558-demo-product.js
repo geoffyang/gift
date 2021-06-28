4d2a4250f8ecdc19b41af25c23496d3235057af0
Geoffrey <geoffreyyang@gmail.com>
2021-06-28 03:37:04 -0400

    new seeds

--- backend/db/seeders/20210621230558-demo-product.js
+++ backend/db/seeders/20210621230558-demo-product.js
@@ -19,6 +19,28 @@ module.exports = {
         longDescription: "Essential nutrients to boost your sleep, vision, cardiovascular and cell functions",
         userId: 1,
         imageUrl: 'https://giftnowbucket.s3.amazonaws.com/1624856943399.png'
+      },
+      {
+        title: 'Childrens Slingshot',
+        // imageUrl: '',
+        shortDescription: "Let your children have fun in the woods.",
+        userId: 1,
+        imageUrl: 'https://giftnowbucket.s3.amazonaws.com/1624857064753.png'
+      },
+      {
+        title: 'Pacific Vacation Shorts',
+        // imageUrl: '',
+        shortDescription: "Head to the pool this weekend with your friends.",
+        userId: 1,
+        imageUrl: 'https://giftnowbucket.s3.amazonaws.com/1624857195682.png'
+      },
+      {
+        title: 'Quality Meats',
+        // imageUrl: '',
+        shortDescription: "Ship this directly to your next barbecue.",
+
+        userId: 1,
+        imageUrl: 'https://giftnowbucket.s3.amazonaws.com/1624857774293.png'
       }
     ], {});
 
